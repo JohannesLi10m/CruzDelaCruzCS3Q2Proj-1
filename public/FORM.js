f2ea6c20c22f57eb896537d3b0251f5e78d404ea
[JohannesLi10m] <[b2029jsadelacruz@pshs.edu.ph]>
2026-04-16 09:51:43 +0800

    commoirgjwerojgiohgoroihgjeiogioerujgioruhy

--- public/FORM.js
+++ public/FORM.js
@@ -2,7 +2,6 @@ let form = document.getElementById("form");
 
 
 form.addEventListener("submit", function(e){
-    alert("Form submitted!");
     e.preventDefault();
 
     let name = document.getElementById("name").value;
@@ -14,16 +13,15 @@ form.addEventListener("submit", function(e){
     let remember = document.getElementById("remember").checked;
 
     if(password !== confirmPassword){
-        document.getElementById("result").innerHTML = "Passwords do not match";
+        document.getElementById("result").innerHTML = "Passwords do not match";//passwords does not match, but form still submits somehow.//
         return;
     }
-
+    alert("Form submitted!");
     if(remember){
         localStorage.setItem("name", name);
         localStorage.setItem("email", email);
         localStorage.setItem("age", age);
         localStorage.setItem("team", team);
-        localStorage.setItem("driver", driver);
     }
 
     window.location.href = "../index.html";
